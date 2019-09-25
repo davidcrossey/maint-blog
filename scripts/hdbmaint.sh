71904bafaae6dbf41a0032594b38c6fea97f22d7
David Crossey <dcrossey@kx.com>
2019-09-25 22:13:15 +1000

    Bash for taskset only

--- scripts/hdbmaint.sh
+++ scripts/hdbmaint.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-
-usage() {
-    echo -e "\nRequirements:\n\t./dbmaint.q\n" \
-            "\nUsage:\n\tbash hdbmaint.sh <hdb_directory>"
-            
-    exit 1
-}
-
-if [ ! $1 ]; then
-    usage
-fi
-
-taskset -c 0 q ./dbmaint.q -database `readlink -f $1` -c 2000 2000 <<< '\l hdbmaint.q'
+taskset -c 0 q ./dbmaint.q $* -c 2000 2000 <<< '\l hdbmaint.q'
 exit 0
