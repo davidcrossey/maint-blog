dec4b11e407d4a65fc80c710aef1e7db4be2881a
David Crossey <dcrossey@kx.com>
2019-09-29 13:03:26 +1000

    Added maint log to disk

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 q setuphdb.q -db hdb
-bash hdbmaint.sh -db ./hdb -action renamecol -table trades -oldname px -newname price
-bash hdbmaint.sh -db ./hdb -action addcol -table trades -colname industry -colvalue 'enlist ""'
-bash hdbmaint.sh -db ./hdb -action fncol -table trades -colname industry -fn '{(` sv x,`sym)?`$y} db'
+bash hdbmaint.sh -db ./hdb -action renamecol -table trades -oldname px -newname price 2>&1 >hdbmaint.log
+bash hdbmaint.sh -db ./hdb -action addcol -table trades -colname industry -fn 'enlist "tech"' 2>&1 >>hdbmaint.log
+bash hdbmaint.sh -db ./hdb -action fncol -table trades -colname industry -fn '{(` sv x,`sym)?`$y} db' 2>&1 >>hdbmaint.log
